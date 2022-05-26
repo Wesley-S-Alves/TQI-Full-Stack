3eda61faf7c35058549f7fa42e855ccff2b69c14
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-05-26 09:46:30 -0300

    delete Modulo 3

--- Modulo 3/Part 1/aula_3.js
+++
@@ -1,8 +0,0 @@
-//funções
-
-function soma (a,b) {// comando function declara uma função
-    console.log(a+b); // imprime no console igual o comando 'print' do python
-    return a+b; // retorna o valor para ser utilizado
-}
-
- soma (3, 5);
