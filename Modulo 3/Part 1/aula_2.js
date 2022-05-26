043a77decec451592ad5072742c9a720bb318f9a
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-05-26 09:16:50 -0300

    Aula 3

---
+++ Modulo 3/Part 1/aula_2.js
@@ -0,0 +1,10 @@
+// adicionando comentários
+
+var x = 1; // comentário de uma linha  ---> //
+
+/* comentário 
+de
+várias
+linhas */
+ var x = 0; // comando var declara uma variável
+ const X = 0; // comando const declara uma constante, sendo escrita em maiúsculo para facilitar a de leitura do código.
