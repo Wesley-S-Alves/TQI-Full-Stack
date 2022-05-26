41bd97a74d22aa731b27fd8c7b9f6c1d01415d7c
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-05-26 09:42:26 -0300

    Corrigindo nome das pastas

--- Modulo 3/Part 1/aula_3.js
+++ Modulo 3/Part 1/aula_3.js
@@ -1,9 +1,8 @@
 //funções
 
 function soma (a,b) {// comando function declara uma função
-    console.log(a+b);
-    return a+b;
+    console.log(a+b); // imprime no console igual o comando 'print' do python
+    return a+b; // retorna o valor para ser utilizado
 }
 
- soma (3,5);
- 
+ soma (3, 5);
