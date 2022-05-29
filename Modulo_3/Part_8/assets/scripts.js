0ccd2c557957b2ec27bb875ffe3c41076a999f21
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-05-29 17:36:57 -0300

    Part_8

---
+++ Modulo_3/Part_8/assets/scripts.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const carBtn = document.getElementById('change-cat');
+const getCats = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+        return json.webpurl;
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+const loadImg = async()=>{
+    const catImg = document.getElementById('cat')
+    catImg.src = await getCats();
+}
+
+carBtn.addEventListener('click', loadImg);
+loadImg();
